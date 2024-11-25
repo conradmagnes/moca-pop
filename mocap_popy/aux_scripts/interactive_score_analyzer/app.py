c1798df9269eaeafffdbe5b22ee2a23427da78b9
conradmagnes <conradm@magnes.ch>
2024-11-25 22:24:26 +0100

    ~move app helper functions to separate file

--- mocap_popy/aux_scripts/interactive_score_analyzer/app.py
+++ mocap_popy/aux_scripts/interactive_score_analyzer/app.py
@@ -33,113 +33,10 @@ from mocap_popy.scripts.unassign_rb_markers.scoring import scorer, scoringParame
 
 import mocap_popy.aux_scripts.interactive_score_analyzer.constants as isa_consts
 import mocap_popy.aux_scripts.interactive_score_analyzer.layout as isa_layout
+import mocap_popy.aux_scripts.interactive_score_analyzer.helpers as isa_helpers
 
 LOGGER = logging.getLogger("InteractiveScoreAnalyzer")
 ## ---- ##
-
-
-def refactor_removal_threshold(
-    scoring_params: scoringParameters.ScoringParameters, marker_names: list[str]
-):
-    """!Refactor the removal threshold to be a dictionary with marker names as keys.
-
-    @param scoring_params Scoring parameters object.
-    @param marker_names List of marker names.
-    """
-    if isinstance(scoring_params.removal_threshold, dict):
-        removal_threshold_all = 0
-        for m in marker_names:
-            if m not in scoring_params.removal_threshold:
-                scoring_params.removal_threshold[m] = 0
-    else:
-        removal_threshold_all = scoring_params.removal_threshold
-        scoring_params.removal_threshold = {
-            m: scoring_params.removal_threshold for m in marker_names
-        }
-
-    return scoring_params, removal_threshold_all
-
-
-def remove_symmetry_from_scoring(scoring_params: scoringParameters.ScoringParameters):
-    """!Remove symmetry from the scoring parameters.
-
-    @param scoring_params Scoring parameters object.
-    """
-    removal_threshold = {}
-    for k, v in scoring_params.removal_threshold.items():
-        try:
-            _, symm_comp = regex.parse_symmetrical_component(k)
-        except ValueError:
-            symm_comp = k
-        removal_threshold[symm_comp] = v
-
-    scoring_params.removal_threshold = removal_threshold
-    return scoring_params
-
-
-def best_fit_transform(target_body: RigidBody, active_body: RigidBody):
-    """
-    Computes the optimal rigid transformation (rotation + translation) that aligns
-    points A with points B using the Kabsch algorithm.
-
-    @param target_body Rigid body to be transformed.
-    @param active_body Rigid body to be used as the reference.
-    """
-    active_nodes = [n for n in active_body.nodes if n.exists]
-    active_markers = [n.marker for n in active_nodes]
-    active_positions = np.array([n.position for n in active_nodes])
-    calibrated_positions = np.array(
-        [target_body.get_node(m).position for m in active_markers]
-    )
-
-    A = calibrated_positions
-    B = active_positions
-
-    # 1. Calculate centroids of A and B
-    centroid_A = np.mean(A, axis=0)
-    centroid_B = np.mean(B, axis=0)
-
-    # 2. Translate points to have their centroids at the origin
-    AA = A - centroid_A
-    BB = B - centroid_B
-
-    # 3. Compute the covariance matrix H
-    H = AA.T @ BB
-
-    # 4. Compute the Singular Value Decomposition (SVD) of H
-    U, S, Vt = np.linalg.svd(H)
-
-    # 5. Compute the optimal rotation matrix R
-    R = Vt.T @ U.T
-
-    # Special reflection case to correct improper rotation matrices
-    if np.linalg.det(R) < 0:
-        Vt[-1, :] *= -1
-        R = Vt.T @ U.T
-
-    # 6. Compute the optimal translation vector t
-    t = centroid_B - R @ centroid_A
-
-    all_calibrated_positions = np.array([n.position for n in target_body.nodes])
-    transformed_points = apply_transform(all_calibrated_positions, R, t)
-    target_body.update_node_positions(
-        {m: p for m, p in zip(target_body.get_markers(), transformed_points)}
-    )
-
-    return
-
-
-def apply_transform(points, R, t):
-    """
-    Applies the given rigid transformation (rotation + translation) to the points.
-
-    @param points (N, 3) ndarray of points to transform.
-    @param R (3, 3) ndarray representing the rotation matrix.
-    @param t (3, ) ndarray representing the translation vector.
-
-    @return transformed_points (N, 3) ndarray of transformed points.
-    """
-    return (R @ points.T).T + t
 
 
 project_dir = os.path.join(directory.DATASET_DIR, "shoe_stepping")
@@ -181,14 +78,14 @@ active_nodes = [
 active_body = copy.deepcopy(calibrated_body)
 active_body.update_node_positions({n.marker: n.position for n in active_nodes})
 
-best_fit_transform(calibrated_body, active_body)
+isa_helpers.best_fit_transform(calibrated_body, active_body)
 
 scoring_params = scoringParameters.ScoringParameters()
-scoring_params, removal_threshold_all = refactor_removal_threshold(
+scoring_params, removal_threshold_all = isa_helpers.refactor_removal_threshold(
     scoring_params, calibrated_body.get_markers()
 )
 if ignore_symmetry:
-    scoring_params = remove_symmetry_from_scoring(scoring_params)
+    scoring_params = isa_helpers.remove_symmetry_from_scoring(scoring_params)
 
 # Initial Node Positions
 initial_positions = {m.marker: m.position for m in active_body.nodes}
@@ -909,12 +806,6 @@ def update_scoring_params_from_file(contents):
     content_type, content_string = contents.split(",")
     decoded = base64.b64decode(content_string)
 
-    # Try to load the content into a dataframe, if it's a CSV or Excel
-    # try:
-    #     json_data = json.loads(decoded.decode("utf-8"))
-    # except Exception as e:
-    #     raise ValueError(f"Error loading JSON file: {e}")
-
     try:
         scoring_params = scoringParameters.ScoringParameters.model_validate_json(
             decoded
@@ -923,11 +814,11 @@ def update_scoring_params_from_file(contents):
         LOGGER.error(f"Error importing scoring parameters: {e}")
         return dash.no_update
 
-    scoring_params, removal_threshold_all = refactor_removal_threshold(
+    scoring_params, removal_threshold_all = isa_helpers.refactor_removal_threshold(
         scoring_params, active_body.get_markers()
     )
     if ignore_symmetry:
-        scoring_params = remove_symmetry_from_scoring(scoring_params)
+        scoring_params = isa_helpers.remove_symmetry_from_scoring(scoring_params)
 
     return scoring_params.model_dump_json(), removal_threshold_all
 
